6f09c7e591ca26280f19d0deb16f0a494918cc32
maauma <120688823+maauma@users.noreply.github.com>
2023-08-04 12:32:37 +0200

    sécurité

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,16 @@
 // Importation du module jsonwebtoken
 const jwt = require('jsonwebtoken');
 
+// Utilisation de la clé secrète stockée dans les variables d'environnement
+const JWT_SECRET = process.env.JWT_SECRET;
+
 // Exportation d'un middleware pour l'authentification basée sur les tokens JWT
 module.exports = (req, res, next) => {
   try {
     // Extraction du token du header d'autorisation
     const token = req.headers.authorization.split(' ')[1];
     // Vérification et décryptage du token
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const decodedToken = jwt.verify(token, JWT_SECRET);
     // Récupération de l'identifiant de l'utilisateur à partir du token décrypté
     const userId = decodedToken.userId;
     // Comparaison de l'identifiant utilisateur dans le corps de la requête avec celui extrait du token
